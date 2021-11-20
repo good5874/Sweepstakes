6f15156b98839c5228fcb61da0ba51769349bf48
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-11-20 21:00:51 +0300

    add CurrencyRepository

---
+++ Sweepstakes.DAL/Repositories/CurrencyRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sweepstakes.Common.Models;
+using Sweepstakes.DAL.Abstracts;
+using Sweepstakes.DAL.Interfaces;
+
+namespace Sweepstakes.DAL.Repositories
+{
+    public class CurrencyRepository : AbstractCRUDRepository<Currency>, ICurrencyRepository
+    {
+        public CurrencyRepository(string conection) : base(conection) { }
+    }
+}
