1ecee058ed2731482a2e81b5807aef269fd62a66
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-11-20 20:47:04 +0300

    add BetRepository

---
+++ Sweepstakes.DAL/Repositories/BetRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sweepstakes.Common.Models;
+using Sweepstakes.DAL.Abstracts;
+using Sweepstakes.DAL.Interfaces;
+
+namespace Sweepstakes.DAL.Repositories
+{
+    public class BetRepository : AbstractCRUDRepository<Bet>, IBetRepository
+    {
+        public BetRepository(string conection) : base(conection) { }
+    }
+}
