516d9df9904f21cb1ff89b0c71d7296a7faee1e6
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-11-21 21:24:09 +0300

    added DataAccessInitializer

---
+++ Sweepstakes.DI/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sweepstakes.DAL.Interfaces;
+using Sweepstakes.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes.DI
+{
+    public static class Startup
+    {
+        public static void DataAccessInitializer(this IServiceCollection services)
+        {
+            services.AddTransient<IBetRepository, BetRepository>();
+            services.AddTransient<ICurrencyRepository, CurrencyRepository>();
+            services.AddTransient<IEventRepository, EventRepository>();
+            services.AddTransient<IRateTeamOnEventRepository, RateTeamOnEventRepository>();
+            services.AddTransient<ISportRepository, SportRepository>();
+            services.AddTransient<ITeamRepository, TeamRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+        }
+    }
+}
