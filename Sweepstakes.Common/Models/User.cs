dac74e8929a0f17dadcbcd93a80a832be57880bf
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-11-21 13:26:40 +0300

    fix name

--- Sweepstakes.Common/Models/User.cs
+++ Sweepstakes.Common/Models/User.cs
@@ -26,7 +26,7 @@ namespace Sweepstakes.Common.Models
         }
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
